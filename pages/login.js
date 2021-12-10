f877c9d6b449ee92a1278c3e7b521542bc1d4ae7
Jarosław Kudzia <jaroslawkudzia73@gmail.com>
2021-12-10 08:15:35 +0100

    s

--- pages/login.js
+++ pages/login.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import LoginForm from '../components/form/LoginForm';
-import { AuthContext } from '../lib/AuthContext';
+import AuthContext from '../lib/AuthContext';
 import { useContext } from 'react';
 
 
 function login() {
 
-     const { user , login } = useContext(AuthContext);
+     const { user, login } = useContext(AuthContext);
+     console.log(user)
      return (
           <div className='w-[100vw] h-[100vh] flex justify-center items-center bg-blue-50'>
                {/* <LoginForm /> */}
-               {/* {login()} */}
+               {login()}
           </div>
      )
 }
