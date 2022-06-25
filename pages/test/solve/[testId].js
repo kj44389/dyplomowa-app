3b2f5b5296083b277e270b9e3cf450ffc516419f
Jarosław Kudzia <jaroslawkudzia73@gmail.com>
2022-06-25 09:43:26 +0200

    fix: fixed infinite hooks loop, and showing warning after getting test results.

--- pages/test/solve/[testId].js
+++ pages/test/solve/[testId].js
@@ -1,6 +1,7 @@
 import Layout from 'components/Layout/Layout';
 import Question from 'components/Layout/LoggedContent/SolveTest/Test/Question';
 import NameEmailForm from 'components/Layout/LoggedContent/SolveTest/CodeNameForm/NameEmailForm';
+import WarningModal from 'components/WarningModal/WarningModal';
 
 import Answer from 'components/Layout/LoggedContent/SolveTest/Test/Answer';
 import ResultsCard from 'components/Layout/LoggedContent/SolveTest/Test/ResultsCard';
@@ -83,16 +84,6 @@ const Solve = ({ testData, testQuestions, testAnswers, fullName, Email }) => {
 	const [warnings, setWarnings] = useState(0);
 
 	useEffect(() => {
-		window.addEventListener('blur', () => {
-			handleModalOpen();
-		});
-	}, []);
-
-	useEffect(() => {
-		if (showModal === true) setWarnings(warnings + 1);
-	}, [showModal, warnings]);
-
-	useEffect(() => {
 		if (answers.length > 0) {
 			setTestState({ ...testState, maxIndex: questions.length - 1 });
 			setTimeLeft(questions[0].question_time);
@@ -121,10 +112,6 @@ const Solve = ({ testData, testQuestions, testAnswers, fullName, Email }) => {
 		setResultsFetched(true);
 	}, [results]);
 
-	const handleModalOpen = () => {
-		setShowModal(true);
-	};
-
 	const handleModalClose = () => {
 		setShowModal(false);
 	};
@@ -132,20 +119,9 @@ const Solve = ({ testData, testQuestions, testAnswers, fullName, Email }) => {
 	const handleFinishTest = async () => {
 		setTimeRunOut(true);
 		setFinishStatus(true);
-		window.removeEventListener('blur', () => null);
-		console.log(
-			'json',
-			JSON.stringify({
-				questions: questions,
-				answers: answers,
-				questionsState: questionsState,
-				test_id: testData.test_id,
-				user_id: user ? user.id : null,
-				user_email: user ? user.email : Email,
-				user_full_name: user ? user.user.name : fullName,
-			})
-		);
-
+		window.addEventListener('blur', () => {
+			setShowModal(false);
+		});
 		await _fetch(`${absoluteUrlPrefix}/api/v2/test/results`, {
 			method: 'POST',
 			headers: { contentType: 'application/json' },
@@ -168,12 +144,20 @@ const Solve = ({ testData, testQuestions, testAnswers, fullName, Email }) => {
 	};
 
 	useEffect(() => {
-		// TODO: fix a bug - when modal is visible there is react loop in hooks.
+		if (showModal == true && finishStatus == false) {
+			setWarnings((prevState) => prevState + 1);
+		}
+	}, [showModal, finishStatus]);
+
+	useEffect(() => {
+		if (warnings === 0) {
+			window.addEventListener('blur', () => {
+				setShowModal(true);
+			});
+		}
 		if (warnings >= 3 && finishStatus === false) {
-			window.removeEventListener('blur', () => null);
 			setShowModal(false);
-			// handleFinishTest();
-			setFinishStatus(true);
+			handleFinishTest();
 		}
 	}, [warnings]);
 
@@ -297,53 +281,7 @@ const Solve = ({ testData, testQuestions, testAnswers, fullName, Email }) => {
 					results.length > 0 && <ResultsCard results={results} testName={testData.test_name} />
 				)}
 			</div>
-			{showModal && (
-				<div className='h-modal fixed top-32 left-1/2 z-50 -translate-x-1/2  items-center justify-center overflow-y-auto overflow-x-hidden sm:h-full ' id='popup-modal'>
-					<div className='relative h-full w-full max-w-md px-4 md:h-auto'>
-						<div className='relative rounded-lg bg-white shadow dark:bg-gray-700'>
-							<div className='flex justify-end p-2'>
-								<button
-									type='button'
-									className='ml-auto inline-flex items-center rounded-lg bg-transparent p-1.5 text-sm text-gray-400 hover:bg-gray-200 hover:text-gray-900 dark:hover:bg-gray-800 dark:hover:text-white'
-									data-modal-toggle='popup-modal'
-									onClick={(e) => {
-										handleModalClose();
-									}}>
-									<svg className='h-5 w-5' fill='currentColor' viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg'>
-										<path
-											fillRule='evenodd'
-											d='M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z'
-											clipRule='evenodd'></path>
-									</svg>
-								</button>
-							</div>
-
-							<div className='p-6 pt-0 text-center'>
-								<svg
-									className='mx-auto mb-4 h-14 w-14 text-red-500 dark:text-red-500'
-									fill='none'
-									stroke='currentColor'
-									viewBox='0 0 24 24'
-									xmlns='http://www.w3.org/2000/svg'>
-									<path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z'></path>
-								</svg>
-								<h3 className='mb-5 text-lg font-normal text-gray-500 dark:text-gray-400'>
-									During test You can`&apos`t use external help. Please finish your test by yourself or Your test will fail.
-								</h3>
-								<button
-									data-modal-toggle='popup-modal'
-									type='button'
-									className='mr-2 inline-flex items-center rounded-lg bg-green-600 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-red-800 focus:ring-4 focus:ring-red-300'
-									onClick={(e) => {
-										handleModalClose();
-									}}>
-									I`&apos`m understand
-								</button>
-							</div>
-						</div>
-					</div>
-				</div>
-			)}
+			{showModal && <WarningModal handleModalClose={handleModalClose} />}
 		</Layout>
 	);
 };
